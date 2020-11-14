1f230b28197433654ca68ef9a03109596355a2ed
Clara Hester <clarahester@Claras-MacBook-Air.local>
2020-11-14 07:49:22 -0600

    11-16-20

---
+++ funciones.js
@@ -0,0 +1,34 @@
+// Suma
+
+const a = 50;
+
+const b = 23;
+
+const suma = a + b;
+
+const resta = a - b;
+
+const texto = 'La voz de tu cabeza siempre tiene 
+el mimso volumen.'
+
+const espacios = ' Hola aquí estoy. '
+
+const email = 'clara.e.hester@gmail.com'
+
+alert(`La suma de ${a} y ${b} es ${suma}.`);
+
+alert(`La resta entre ${a} y ${b} es ${resta}.`);
+
+console.log(texto);
+
+console.log(texto.toUpperCase());
+
+console.log(texto.toLowerCase());
+
+console.log(espacios);
+
+console.log(espacios.trim());
+
+console.log(email);
+
+console.log (email.split('@'));
